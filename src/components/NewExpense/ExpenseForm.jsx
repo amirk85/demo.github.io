71ff9425e51923c5002617815e38dff4183008f7
tohit64 <khanamir8589@gmail.com>
2021-12-28 12:53:09 +0530

    Updated ExpenseForm

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -17,9 +17,9 @@ function ExpenseForm() {
           <label>Date</label>
           <input type="date" min="2019-01-01" max="2022-12-31" />
         </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add New Expenses</button>
-        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add New Expenses</button>
       </div>
     </form>
   );
